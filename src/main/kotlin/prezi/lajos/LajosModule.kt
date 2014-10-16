1dc221b6d15588cdb27f5f1f523774dcedd75f8b
Lorant Pinter <lorant.pinter@prezi.com>
2014-10-16 22:50:29 +0200

    Add a struct

--- src/main/kotlin/prezi/lajos/LajosModule.kt
+++ src/main/kotlin/prezi/lajos/LajosModule.kt
@@ -9,5 +9,14 @@ object LajosModule {
 		console.log("This is Spaghetti " + Spaghetti.getSpaghettiVersion())
 		console.log("10 + 20 = : " + makeMeADummy().add(10, 20))
 		console.log("Ordinal of ALMA: " + Values.ALMA + " (something = " + Constants.SOMETHING + ")")
+		var point = object {
+			val x = 10
+			val y = 20
+			fun length():Int {
+				val square = x * x + y * y
+				return Math.sqrt(square.toDouble()).toInt()
+			}
+		}
+		console.log("Point", point, "length =", point.length());
 	}
 }
