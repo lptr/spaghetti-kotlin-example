624462087871e4194ab0603c36f3d1bc4be05c3e
Lorant Pinter <lorant.pinter@prezi.com>
2014-10-16 22:55:09 +0200

    Add some generics

--- src/main/kotlin/prezi/lajos/LajosModule.kt
+++ src/main/kotlin/prezi/lajos/LajosModule.kt
@@ -9,14 +9,14 @@ object LajosModule {
 		console.log("This is Spaghetti " + Spaghetti.getSpaghettiVersion())
 		console.log("10 + 20 = : " + makeMeADummy().add(10, 20))
 		console.log("Ordinal of ALMA: " + Values.ALMA + " (something = " + Constants.SOMETHING + ")")
-		var point = object {
+		var point:Point<Int> = object {
 			val x = 10
 			val y = 20
 			fun length():Int {
 				val square = x * x + y * y
 				return Math.sqrt(square.toDouble()).toInt()
 			}
-		}
+		} as Point<Int>
 		console.log("Point", point, "length =", point.length());
 	}
 }
